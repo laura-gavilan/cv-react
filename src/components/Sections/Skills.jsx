52bcd2c1a8d7d3d1f0cfbd4e33464b5314712703
Laura Gavilan <lauragavilansabiote@hotmail.com>
2025-07-28 19:09:30 +0200

    feat(Sections): add, education, experience, languaje, skills components and sections styles

---
+++ src/components/Sections/Skills.jsx
@@ -0,0 +1,16 @@
+import "./Sections.css"
+
+export const Skills = () => {
+    return (
+        <section className="skills">
+            <img src="./src/media/images/html5.png" alt="icono de HTML5" />
+
+            <img src="./src/media/images/css3.png" alt="icono de CSS3" />
+
+            <img src="./src/media/images/js.png" alt="icono de JS" />
+
+            <img src="./src/media/images/react.png" alt="icono de React" />
+            
+        </section>
+    )
+}
